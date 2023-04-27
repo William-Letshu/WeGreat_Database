202ce2bbfa7164bba06f6b946a24c3d16d9e5a23
William-Letshu <123249866+William-Letshu@users.noreply.github.com>
2023-04-27 20:07:48 +0200

    Updated schema

--- schema_v0.sql
+++ schema_v0.sql
@@ -23,7 +23,6 @@ CREATE TABLE merchants (
     first_name VARCHAR(255) NOT NULL,
     second_names VARCHAR(255),
     surname VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(255),
     description TEXT,
     police_clearance BOOLEAN NOT NULL DEFAULT false,
     verified BOOLEAN NOT NULL DEFAULT false,
